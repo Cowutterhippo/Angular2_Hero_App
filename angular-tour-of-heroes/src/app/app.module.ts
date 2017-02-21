d07e2b26b1f6491c5f2a5419b5b3d693c7342d20
Cowutterhippo <andrew.kolansky#gmail.com>
2017-02-21 14:23:10 -0500

    Weekend Project working Angular 2 Heroes APP

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,15 +1,39 @@
-import { NgModule }      from '@angular/core';
+ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { AppComponent }  from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+
+// rxjs is a library for working wiht observables.
+import './rxjs-extensions';
+import { AppComponent } from './app.component';
+import { AppRoutingModule, routedComponents } from './app-routing.module';
+import { HeroService } from './hero.service';
+import { HeroSearchComponent } from './hero-search.component';
+
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 600 })
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroSearchComponent,
+    routedComponents
   ],
-  bootstrap: [ AppComponent ]
+  providers: [
+    HeroService
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// Rather than require a real API server, this example simulates communication with 
+// the remote server by adding the InMemoryWebApiModule to the module imports, 
+// effectively replacing the Http client's XHR backend service with an in-memory alternative.
